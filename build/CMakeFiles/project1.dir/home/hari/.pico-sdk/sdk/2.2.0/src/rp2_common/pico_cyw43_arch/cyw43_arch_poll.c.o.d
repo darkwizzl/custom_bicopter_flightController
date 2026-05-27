c126233b11122787827538bde1d5389484fa8f66
darkwizzl <harikrishnansunil120@gmail.com>
2026-05-27 11:00:34 +0530

    remove build artifacts

--- build/CMakeFiles/project1.dir/home/hari/.pico-sdk/sdk/2.2.0/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/project1.dir/home/hari/.pico-sdk/sdk/2.2.0/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c.o: \
- /home/hari/.pico-sdk/sdk/2.2.0/src/rp2_common/pico_cyw43_arch/cyw43_arch_poll.c
--- build/_deps/picotool-build/data_locs.cpp
+++
@@ -1,7 +0,0 @@
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> data_locs = {
-    "./","/home/hari/Desktop/bicopter/build/_deps/picotool/"
-};
